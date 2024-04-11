ffc269d14d8e3e7f1b9bda6c0837fa204e0aab93
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-04-11 13:28:17 -0400

    Added Game Manager and Event Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Core.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+        
+
+        private void Awake()
+        {
+            // Create a singleton of the game manager object type.
+            if (Instance && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+
+    public sealed class StartSessionEvent : GameEvent
+    {
+    }
+    public sealed class EndSessionEvent : GameEvent
+    {
+    }
+}

e724d6631f5666a702bcfdd8ca9dad2f4bd5e952
Tyare <tytydaniels352@gmail.com>
2024-04-10 22:15:52 -0400

    Squashed commit of the following:
    commit c033b613f0a96232c35d72e92aec4be6749a7799
    Merge: 85424f6 168a65d
    Author: Tyare <84937548+Tyrizzy@users.noreply.github.com>
    Date:   Wed Apr 10 21:58:00 2024 -0400
    
        Merge pull request #7 from RohitK-RIT/framework-port
    
        organized file hierarchy, ported RTS framework
    
    commit 168a65d33cdd9702a62a319a94fc67d71af513dd
    Author: Abis <26350896+TheOneAbis@users.noreply.github.com>
    Date:   Wed Apr 10 20:47:57 2024 -0400
    
        organized file hierarchy, ported RTS framework
    
        Check my test scene for how it's being used
    
    commit 85424f6a279f0f293db8968133d49ef7a094f889
    Author: RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
    Date:   Wed Apr 10 19:20:38 2024 -0400
    
        Create .gitattributes
    
        Credits to Hextant Studios
    
    commit 3a72f07ecae307f20e0b3c87b244ad8a343d883e
    Author: RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
    Date:   Wed Apr 10 19:17:38 2024 -0400
    
        Create pull_request_template.md

---
+++ Assets/Scripts/NewAbility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewAbility : UnitAbility
+{
+    public override void Execute()
+    {
+        base.Execute();
+    }
+}

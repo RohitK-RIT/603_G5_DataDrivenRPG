5f1a5eaa8ca77a65db5e6b13651739ad13397ac7
1TaodeOgden1 <70358247+1TaodeOgden1@users.noreply.github.com>
2024-04-12 13:41:23 -0400

    added basic menus, ability to pause

---
+++ Assets/PauseMenuController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenuController : MonoBehaviour
+{
+    private GameObject pauseUI;
+    public bool isPaused;
+
+     void Start()
+     {
+        //game is assumed to start unpaused 
+        pauseUI = GameObject.Find("PauseMenu");
+        pauseUI.SetActive(false);
+        isPaused = false; 
+     }
+
+    public void TogglePause()
+    {
+        //toggle on
+        if (!isPaused)
+        {
+            pauseUI.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0;
+        }
+        //toggle off
+        else if (isPaused)
+        {
+            pauseUI.SetActive(false);
+            isPaused = false; 
+            Time.timeScale = 1; 
+        }
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene("LevelPrototype");
+    }
+
+    public void ShowOptions()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+
+    //when the Pause key is pressed
+    public void OnPause()
+    {
+        TogglePause();
+    }
+
+   
+}

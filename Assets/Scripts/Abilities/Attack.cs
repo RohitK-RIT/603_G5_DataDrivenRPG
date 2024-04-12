78044f85dd88ee3f3ca3e24d25ec0783167ce84f
Abis <26350896+TheOneAbis@users.noreply.github.com>
2024-04-12 19:22:18 -0400

    fixed friendly unit attack script to raycast to check if target is still in range upon ability execution

--- Assets/Scripts/Abilities/Attack.cs
+++ Assets/Scripts/Abilities/Attack.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Attack : UnitAbility
 {
     public float atkDamage = 25f;
+    public float atkRange = 50f;
 
     Unit target;
 
     public override void Execute()
     {
         if (target)
-            target.TakeDamage(atkDamage);
+        {
+            // Raycast towards the target. If it hits an enemy unit (which may not necessarily be the target), deal dmg to it.
+            if (Physics.Raycast(transform.position, target.transform.position - transform.position, out RaycastHit hit, atkRange))
+            {
+                if (hit.collider.TryGetComponent(out Unit u))
+                {
+                    if (u.Hostility != GetComponent<Unit>().Hostility)
+                        u.TakeDamage(atkDamage);
+                }
+            }
+        }
 
         base.Execute();
     }

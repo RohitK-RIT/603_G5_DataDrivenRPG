bf93f9ff34ae319e69fdeffa6868cf76e8ae42f2
Tyare <tytydaniels352@gmail.com>
2024-04-16 05:40:14 -0400

    visuals for attacking

--- Assets/Scripts/Abilities/Attack.cs
+++ Assets/Scripts/Abilities/Attack.cs
@@ -12,10 +12,6 @@ public class Attack : UnitAbility
     //Added by Ty
     LineRenderer Line;
     Transform LineOrigin;
-    Ray ray;
-    RaycastHit[] hits = new RaycastHit[2];
-    bool isHalfCover = false;
-    bool isHit = false;
 
     private void Start()
     {
@@ -33,47 +29,15 @@ public class Attack : UnitAbility
     {
         if (target)
         {
-            /*
             // Raycast towards target; deal dmg to whatever is hit (which may not be the target if another enemy unit is in the way)
             if (Physics.Raycast(transform.position, target.transform.position - transform.position, out RaycastHit hit, atkRange, ~(1 << 6)))
             {
-                if (hit.collider.tag == "HalfCover")
+                if (hit.collider.TryGetComponent(out Unit u))
                 {
-                    if (hit.collider.TryGetComponent(out Unit u))
-                    {
-                        u.TakeDamage(atkDamage / 2);
-                        Line.SetPosition(1, hit.collider.transform.position);
-                        StartCoroutine(shootLine());
-                        Debug.Log("Hit through halfcover");
-                    }
-                    Debug.Log("Shot through halfcover");
+                    u.TakeDamage(atkDamage);
+                    Line.SetPosition(1, hit.collider.transform.position);
+                    StartCoroutine(shootLine());
                 }
-            }
-            */
-            ray = new Ray(transform.position, target.transform.position - transform.position);
-            int numHits = Physics.RaycastNonAlloc(ray, hits);
-            if (numHits > 0)
-            {
-                for (int i = 0; i < numHits; i++)
-                {
-                    if (hits[i].collider.gameObject.layer == 7)
-                    {
-                        isHit = true;
-                    }
-                    else if (hits[i].collider.gameObject.layer == 8)
-                    {
-                        isHalfCover = true;
-                    }
-
-                    Debug.Log(hits[i].collider.gameObject.layer);
-                }
-            }
-            if ((hits[1].collider.TryGetComponent(out Unit u) && isHit && isHalfCover))
-            {
-                u.TakeDamage(atkDamage / 2);
-                Line.SetPosition(1, hits[0].collider.transform.position);
-                StartCoroutine(shootLine());
-                Debug.Log("Hit through halfcover");
             }
         }
         base.Execute();

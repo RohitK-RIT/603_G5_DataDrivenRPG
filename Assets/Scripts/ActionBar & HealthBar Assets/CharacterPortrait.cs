de3258146b4f9421899397b92b0991e48b75c237
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-04-18 14:59:13 -0400

    Small refactor

--- Assets/Scripts/ActionBar & HealthBar Assets/CharacterPortrait.cs
+++ Assets/Scripts/ActionBar & HealthBar Assets/CharacterPortrait.cs
@@ -1,16 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterPortrait : MonoBehaviour
 {
     private Unit _owner;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnCharacterPortraitClicked);
+    }
 
     public void SetOwner(Unit owner)
     {
         _owner = owner;
     }
 
-    public void OnCharacterPortraitClicked()
+    private void OnCharacterPortraitClicked()
     {
         SelectionManager.SetSelectedUnits(new List<Unit> { _owner });
     }

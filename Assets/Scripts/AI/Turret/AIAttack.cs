a8a76a167e23002fe98b9208bc3aaf07255ff13e
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-04-19 00:11:22 -0400

    Now a line gets rendered when AI Turret shoots

--- Assets/Scripts/AI/Turret/AIAttack.cs
+++ Assets/Scripts/AI/Turret/AIAttack.cs
@@ -7,16 +7,27 @@ namespace AI.Turret
     {
         public float atkDamage = 25f;
 
-        Unit target;
+        private LineRenderer Line => _line ??= GetComponent<LineRenderer>();
+
+        private Unit _target;
+        private LineRenderer _line;
+
+
+        private void Start()
+        {
+            Line.enabled = false;
+        }
 
         public override void Execute()
         {
-            if(!GetComponent<AIPerception>().VisibleUnits.Contains(target))
+            if (!GetComponent<AIPerception>().VisibleUnits.Contains(_target))
                 return;
-            
-            if (target)
+
+            if (_target)
             {
-                target.TakeDamage(atkDamage);
+                _target.TakeDamage(atkDamage);
+                Line.SetPosition(1, _target.transform.position);
+                StartCoroutine(shootLine());
             }
 
             base.Execute();
@@ -24,8 +35,21 @@ namespace AI.Turret
 
         public void Queue(Unit unit)
         {
-            target = unit;
+            _target = unit;
             base.Queue();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            Line.SetPosition(0, transform.position);
+        }
+
+        IEnumerator shootLine()
+        {
+            Line.enabled = true;
+            yield return new WaitForSeconds(.5f);
+            Line.enabled = false;
         }
     }
 }
